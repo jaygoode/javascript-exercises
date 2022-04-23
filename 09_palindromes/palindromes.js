f7b24e3005cc6ef3cf5e1edb587e38d7a59e6b5b
johnny <john_newcreek@protonmail.com>
2022-04-23 11:51:05 +0300

    completed challenge, all tests passed

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,7 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    strOnlyLetters = str.replace(/[^A-Za-z0-9]/g, "").toLowerCase();
+    stringReverse = strOnlyLetters.split("").reverse().join("").toLowerCase();
+    return stringReverse == strOnlyLetters;
 };
 
 // Do not edit below this line
