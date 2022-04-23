a94b1d9f3d317780b1ac5c80caa43035fb13717b
johnny <john_newcreek@protonmail.com>
2022-04-23 14:08:48 +0300

    completed with .map function

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,25 @@
-const getTheTitles = function() {
+const books = [
+    {
+      title: 'Book',
+      author: 'Name'
+    },
+    {
+      title: 'Book2',
+      author: 'Name2'
+    },
+    {
+        title: 'Book',
+        author: 'Name'
+      },
+      {
+        title: 'Book2',
+        author: 'Name2'
+      }
+  ]
 
+const getTheTitles = function(array) {
+return array.map(book=>book.title);
 };
-
+// getTheTitles(books);
 // Do not edit below this line
 module.exports = getTheTitles;
