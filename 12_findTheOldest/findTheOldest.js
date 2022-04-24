efb2083cfef77b2dd8c4e97b1b55b53a326d4f5a
johnny <john_newcreek@protonmail.com>
2022-04-24 16:32:29 +0300

    completed used reduce()

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,37 @@
-const findTheOldest = function() {
+const people = [
+    {
+      name: "Carly",
+      yearOfBirth: 1942,
+      yearOfDeath: 1970,
+    },
+    {
+      name: "Ray",
+      yearOfBirth: 1962,
+      yearOfDeath: 2011,
+    },
+    {
+      name: "Jane",
+      yearOfBirth: 1912,
+      yearOfDeath: 1941,
+    },
+  ]
 
+const findTheOldest = function(array) {
+    return array.reduce((a, b) => {
+        const lastPerson = getAge(a.yearOfBirth, a.yearOfDeath);
+        const nextPerson = getAge(b.yearOfBirth, b.yearOfDeath);
+        return lastPerson < nextPerson ? b : a;
+    })
 };
+
+const getAge = (birth, death) => {
+    if(!death) {
+        death = new Date().getFullYear();
+    }
+    return death - birth;
+}
+
+// findTheOldest(people);
 
 // Do not edit below this line
 module.exports = findTheOldest;
